8b9d54676c7ce77e0ac0861aa64c02924cdf2af8
Erremme <Erremme2.0@gmail.com>
2025-03-11 15:33:21 +0100

    feat : added bookController and bookRouter

---
+++ controllers/bookController.js
@@ -0,0 +1,13 @@
+//Dati del database
+const connection = require("../data/db")
+
+//INDEX
+const index = (req, res) =>{}
+
+//SHOW
+const show = (req, res) =>{}
+
+//DESTROY
+const destroy = (req, res) =>{}
+
+module.exports={index, show, destroy}
