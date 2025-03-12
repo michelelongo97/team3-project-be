323f72b9ad688473f72b841b57285c81a691cc85
Erremme <Erremme2.0@gmail.com>
2025-03-12 11:46:25 +0100

    feat : added endpoint index to movieController

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -2,7 +2,30 @@
 const connection = require("../data/db")
 
 //INDEX
-const index = (req, res) =>{}
+const index = (req, res) =>{
+
+    const sql = `SELECT title , year_of_release , editor, original_title, year_edition, image, author
+                FROM books
+                WHERE year_edition > 2020`
+    
+    //lanciare la query
+    connection.execute(sql, (err, results) => {
+        if(err){
+            return res.status(500).json({
+                error:"Query Error",
+                message:"Database query failed"
+            });
+        }
+        const books = results.map((book) => {
+            book.image =`${process.env.BE_URL}/books/${book.image}`
+            return book;
+         })
+        
+         res.json(books);
+        
+    }) 
+   
+}
 
 //SHOW
 const show = (req, res) =>{}
