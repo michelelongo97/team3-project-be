5094fcd53222c321245190a9d43c73e352a98f2b
Erremme <Erremme2.0@gmail.com>
2025-03-12 12:54:54 +0100

    added endpoint show and discount

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -28,7 +28,47 @@ const index = (req, res) =>{
 }
 
 //SHOW
-const show = (req, res) =>{}
+const show = (req, res) =>{
+    const {id} = req.params
+    const bookSql = `
+    SELECT books.*,genres.*,discounts.value AS discount_percentage,
+    CASE 
+        WHEN discounts.value IS NOT NULL 
+        THEN ROUND(books.price - (books.price * discounts.value / 100), 2)
+    END AS discounted_price
+    FROM books
+    LEFT JOIN 
+    discounts ON books.id = discounts.book_id
+    JOIN 
+    genres ON genres.id = books.genre_id
+    WHERE 
+    books.id = 150; `; 
+
+    //lanciare la query
+    connection.execute(bookSql, [id], (err, result) => {
+        if(err){
+            return res.status(500).json({
+                error:"Query Error",
+                message:"Database query failed"
+            })
+        }
+
+        
+        const book = result[0]
+
+        if(!book){
+            return res.status(404).json({
+                error: "not found",
+                message:"movie not found"
+            })
+        }
+
+       book.image =`${process.env.BE_URL}/movies/${book.image}`
+
+       res.json(book)           
+})
+   
+}
 
 //DESTROY
 const destroy = (req, res) =>{}
