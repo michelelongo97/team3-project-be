28b2e8951159d7130d4b54538a489730483aa8bb
erikaCutugno <erikucciapop@gmail.com>
2025-03-12 11:53:34 +0100

    add search in controllers

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,13 +1,39 @@
 //Dati del database
-const connection = require("../data/db")
+const connection = require("../data/db");
 
 //INDEX
-const index = (req, res) =>{}
+const index = (req, res) => {};
 
 //SHOW
-const show = (req, res) =>{}
+const showSearch = (req, res) => {
+  const searchInput = req.query.q;
+
+  const sql = `
+    SELECT books.*, genres.category
+    FROM books
+    JOIN genres ON books.genre_id = genres.id
+    WHERE books.title LIKE ?
+    OR books.author LIKE ?
+    OR genres.category LIKE ?
+    `;
+  //Faccio una ricerca parziale con %
+
+  const searchSql = `%${searchInput}%`;
+
+  connection.query(sql, [searchSql, searchSql, searchSql], (err, results) => {
+    if (err) {
+      return res.status(500).json({
+        error: "Query Error",
+        message: `Database query failed: ${sql}`,
+      });
+    }
+    res.json(results);
+  });
+};
+//SHOW
+const show = (req, res) => {};
 
 //DESTROY
-const destroy = (req, res) =>{}
+const destroy = (req, res) => {};
 
-module.exports={index, show, destroy}
+module.exports = { index, showSearch, show, destroy };
