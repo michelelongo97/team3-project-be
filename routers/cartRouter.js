4026766ba645acaa949ea0cd6aea8760412ce810
Erremme <Erremme2.0@gmail.com>
2025-03-14 16:12:50 +0100

    fix: add to cart and added endpoint delete fromCart

--- routers/cartRouter.js
+++ routers/cartRouter.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const router = express.Router();
 
-const addToCart = require("../controllers/cartController");
+const {addToCart , removeFromCart }= require("../controllers/cartController");
+
 
 router.post('/cart', addToCart);
+
+// Rotta per rimuovere un libro dal carrello
+router.delete('/cart/remove/:book_id/:quantity', (req, res) => {
+    const { book_id, quantity } = req.params;
+    removeFromCart(book_id, quantity, res); // Passa i parametri alla funzione
+});
 
 module.exports = router;
