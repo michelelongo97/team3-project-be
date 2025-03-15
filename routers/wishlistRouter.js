29ba6105d76e0bceb38b994e7a2d0f837c8efc31
erikaCutugno <erikucciapop@gmail.com>
2025-03-15 12:46:01 +0100

    add controll userId in wishlist

--- routers/wishlistRouter.js
+++ routers/wishlistRouter.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const wishlistController = require("../controllers/wishlistController");
 
 // Rotte per la wishlist
+router.get("/get-user-id", wishlistController.getUserId);
 router.post("/", wishlistController.addToWishlist);
 router.delete("/", wishlistController.removeFromWishlist);
 
