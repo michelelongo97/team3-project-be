bf787a0abd44945f1903e964ec245a92c4bc290d
alessandrasinigaglia <alessandra.sinigaglia06@gmail.com>
2025-03-15 14:25:54 +0100

    feat: add related books

---
+++ controllers/relatedBooksController.js
@@ -0,0 +1,37 @@
+const connection = require("../data/db");
+
+const getRelatedBooks = (req, res) => {
+  const { id } = req.params; // Prende l'ID del libro dalla rotta
+
+  const sql = `
+    SELECT DISTINCT b.*
+    FROM books b
+    JOIN books main_book ON main_book.id = ?
+    WHERE 
+    (b.genre_id = main_book.genre_id OR b.author = main_book.author)
+    AND b.id != ?;
+  `;
+
+  connection.execute(sql, [id, id], (err, results) => {
+    if (err) {
+      console.error("Errore nella query:", err);
+      return res.status(500).json({
+        error: "Query Error",
+        message: "Database query failed",
+      });
+    }
+
+    console.log("Libri correlati trovati:", results);
+
+    const relatedBooks = results.map((book) => {
+      book.image = `${process.env.BE_URL}/book_cover/${book.image}`;
+      return book;
+    });
+
+    res.json(relatedBooks);
+  });
+};
+
+module.exports = { getRelatedBooks };
+
+
--- routers/bookRouter.js
+++ routers/bookRouter.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 //Import dei controller
 const bookController = require("../controllers/bookController");
+const relatedBooksController = require("../controllers/relatedBooksController");
 
 //Index
 router.get("/", bookController.index);
@@ -12,6 +13,9 @@ router.get("/search", bookController.showSearch);
 
 //Show
 router.get("/:id", bookController.show);
+
+//Related books
+router.get("/related-books/:id", relatedBooksController.getRelatedBooks); 
 
 //Destroy
 router.delete("/:id", bookController.destroy);
